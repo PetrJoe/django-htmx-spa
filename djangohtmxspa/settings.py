2f844a68bfb1dcddf01639fbbf7b6e832905d9a0
Akanmu01 <akinjossy1298@gmail.com>
2022-02-07 00:40:05 +0100

    first commit

--- djangohtmxspa/settings.py
+++ djangohtmxspa/settings.py
@@ -13,6 +13,19 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -57,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
 ]
 
 ROOT_URLCONF = 'djangohtmxspa.urls'
@@ -140,3 +154,4 @@ MEDIAFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
 
 LOGIN_REDIRECT_URL = "home"  # change to desired url name
 LOGOUT_REDIRECT_URL = "home"  # change to desired url name
+
