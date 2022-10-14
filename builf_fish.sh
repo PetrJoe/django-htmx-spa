8adc636f8753c44f3c54828d6bc95c7b3239c4e7
Akanmu Akinkunmi <56254171+Akanmu01@users.noreply.github.com>
2022-10-14 17:15:33 +0100

    Update builf_fish.sh

--- builf_fish.sh
+++ builf_fish.sh
@@ -1,27 +1,2 @@
-{
-  "version": 2,
-  "builds": [
-    {
-      "src": "djangohtmxspa/wsgi.py",
-      "use": "@vercel/python",
-      "config": { "maxLambdaSize": "15mb", "runtime": "python3.9" }
-    },
-    {
-      "src": "build_files.sh",
-      "use": "@vercel/static-build",
-      "config": {
-        "distDir": "staticfiles_build"
-      }
-    }
-  ],
-  "routes": [
-    {
-      "src": "/static/(.*)",
-      "dest": "/static/$1"
-    },
-    {
-      "src": "/(.*)",
-      "dest": "djangohtmxspa /wsgi.py"
-    }
-  ]
-}
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
